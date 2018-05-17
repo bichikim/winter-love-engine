bee8f4c664be4f53e59d62370a9bd8ff77c395ed
Bichi Kim <bichi@live.co.kr>
2018-05-17 23:47:52 +0900

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const winterLove = require('winter-love')
+const {join} = require('path')
+const DEFAULT_PORT = 8080
+const PORT = process.env.PORT || DEFAULT_PORT
+const app = express()
+app.enable('trust proxy')
+
+async function run() {
+  const winterLoveMiddleware = await winterLove({
+    build: true,
+    config: {
+      dev: false,
+      srcDir: join(__dirname, 'node_modules/winter-love/src'),
+    },
+  })
+  app.use(winterLoveMiddleware)
+  await new Promise((resolve) => {
+    app.listen(PORT, resolve)
+  })
+}
+
+
+run().then(() => (console.log('sever is running now')))
